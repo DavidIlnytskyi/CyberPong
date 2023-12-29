ee2d9997384caef28d32db2a453583fbe9c5f216
olenaazarova <116667018+olenaazarova@users.noreply.github.com>
2023-12-29 12:13:47 +0200

    Update motorController.c

--- CyberPong.cydsn/motorController.c
+++ CyberPong.cydsn/motorController.c
@@ -1,29 +1,46 @@
 #include "motorController.h"
 #include "stdlib.h"
+#include "main_data.h"
 
+#include "stdio.h"
 void chooseSide(uint8 cycles, uint8 sideCoord);
-void oneColor();
 
 uint8 stopCoords[] = {1, 7, 13, 19};
 
 extern const uint32 StripLights_CLUT[ ];
 
 
-
 void turnDown(void){
+    PWM_Down_WriteCompare(720);
+    ENA_P_16_Write(1);
     IN1_P_01_Write(1);
     IN2_P_00_Write(0);
 }
 
 void turnUp(void){
+    PWM_Down_WriteCompare(720);
+    ENA_P_16_Write(1);
     IN1_P_01_Write(0);
     IN2_P_00_Write(1);
 }
 
 void stopMoving(){
+    ENA_P_16_Write(0);
     PWM_Down_WriteCompare(0);
 }
-
+void oneColor()
+{
+    StripLights_DisplayClear(StripLights_BLACK);
+    StripLights_MemClear(StripLights_BLACK);
+    
+    StripLights_Pixel(0, 0, StripLights_BLACK);
+    for(int i = 0; i < 24; ++i)
+    {
+        StripLights_Pixel(i, 0, StripLights_CYAN);
+    }
+    CyDelay(10);
+    StripLights_Trigger(1); 
+}
 void setSpin (int motors[]){
     if(motors[0] && motors[1] && motors[2] && motors[3])
     {
@@ -77,16 +94,14 @@ void SetMotorsEnabled(bool enabled) {
     motorsEnabled = enabled;
 }
 
-
-
-void chooseSide(uint8 cycles, uint8 sideCoord)
-{
+void chooseSide(uint8 cycles, uint8 sideCoord){
+    StripLights_DisplayClear(StripLights_BLACK);
+    StripLights_MemClear(StripLights_BLACK);
+    
+    StripLights_Pixel(0, 0, StripLights_BLACK);
     uint32 ledPosition = 1;
     uint8 currCycles = 0;
     uint32 ledPos;
-    
-    StripLights_DisplayClear(StripLights_BLACK);
-	StripLights_MemClear(StripLights_BLACK);
     
     for(int i = 0; i < 7; ++i)
     {
@@ -129,20 +144,108 @@ void chooseSide(uint8 cycles, uint8 sideCoord)
             break;
         }
     }
+
 }
 
-
-void oneColor()
-{
-    StripLights_DisplayClear(StripLights_BLACK);
-	StripLights_MemClear(StripLights_BLACK);
+void setDesiredVoltage(uint16 desired_voltage){
+    volatile int16 adcResult = 0;
+    volatile int16 voltage = 0;
+    ADC_Start();
+    ADC_StartConvert();
+    adcResult = ADC_GetResult16(0);
+    voltage = ADC_CountsTo_mVolts(0, adcResult);
     
-    StripLights_Pixel(0, 0, StripLights_BLACK);
-    for(int i = 0; i < 24; ++i)
-    {
-        StripLights_Pixel(i, 0, StripLights_CYAN);
+
+    UART_UartPutString("readyyyy");
+    if (abs(voltage - desired_voltage) > 90){
+        Pin_Output_Serve_Write(0);    
+        Pin_Motor_Write(0);
+        if (voltage > desired_voltage){               
+            while (voltage > desired_voltage){
+                turnUp();
+                
+                char mes[100];
+                sprintf(mes, "voltage: %u\r\n", voltage );
+                UART_UartPutString(mes);
+                ADC_StartConvert();
+                voltage = ADC_CountsTo_mVolts(0, ADC_GetResult16(0));
+            }                  
+        } else if (voltage < desired_voltage){
+            
+            while (desired_voltage > voltage){
+                turnDown();
+                
+                char mes[100];
+                sprintf(mes, "voltage: %u\r\n", voltage );
+                UART_UartPutString(mes);
+                ADC_StartConvert();
+                voltage = ADC_CountsTo_mVolts(0, ADC_GetResult16(0));
+            }             
+        }
+        stopMoving();
+        CyDelay(150); 
+        Pin_Motor_Write(1);
+        Pin_Output_Serve_Write(1);
     }
-    CyDelay(10);
-    StripLights_Trigger(1); 
 }
+
+bool checkDifficultyChange(){
+    return oldDifficulty != newDiffuculty;
+};
+
+void setDifficulty(){
+    char mes[100];
+    sprintf(mes, "counter: %u\r\n", ballCounter);
+    UART_UartPutString(mes);
+    if (checkDifficultyChange()){
+        ballCounter = 0;
+        oldDifficulty = newDiffuculty;
+    }
+    switch(oldDifficulty){
+        case 0:{
+            easyMode();
+            break;
+        }
+        case 1:{
+            mediumMode();
+            break;
+        }
+        case 2:{
+            hardMode();
+            break;
+        }
+    }
+}
+
+
+void easyMode(){
+    setDesiredVoltage(1950);
+    if(ballCounter < 10){
+        setDesiredVoltage(1950);
+      //  CyDelay(300);
+    } else{
+        ballCounter = 0;
+    }
+    
+}
+
+void mediumMode(){
+    if(ballCounter < 5){
+        setDesiredVoltage(1810);
+    } else if(ballCounter < 10){
+        setDesiredVoltage(1930);
+    } else {
+        ballCounter = 0;
+    }
+    
+}
+void hardMode(){
+    uint16 voltages [] = {1820, 1820, 1900, 1900, 2050, 2050,2050, 2050, 1900, 1900};
+    if (ballCounter < 10){
+        setDesiredVoltage(voltages[ballCounter]);
+    } else{
+        ballCounter = 0;
+    }
+}
+
 /* [] END OF FILE */
